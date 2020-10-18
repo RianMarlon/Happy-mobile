5a24653bb321d1159a62c9c6d84d3512247f6485
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-18 01:14:49 -0300

    Adicionando a estrutura da página OrphanageDetails no projeto mobile

---
+++ src/pages/OrphanageDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+function OrphanageDetails() {
+  return (
+    <View style={styles.container} />
+  );
+}
+
+export default OrphanageDetails;
