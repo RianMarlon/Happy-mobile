b29289a39eddf825ac6345cbcad21dbd749303b1
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-27 14:50:26 -0300

    Adicionando a fonte Nunito_600SemiBold no App.tsx do projeto mobile

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useFonts } from 'expo-font';
-import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import { Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import Routes from './src/routes';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
     Nunito_600SemiBold,
     Nunito_700Bold,
     Nunito_800ExtraBold 
