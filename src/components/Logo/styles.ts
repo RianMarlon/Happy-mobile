12f2aa2ac0d2036eac7ca6d0eed29277b590f5e5
Rian Marlon <rianmarlon20@yahoo.com>
2020-11-12 15:13:38 -0300

    Adicionando o estilo do componente Logo do projeto mobile

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+  },
+
+  logoImg: {
+    width: 166,
+    height: 150,
+  },
+});
+
+export default styles;
