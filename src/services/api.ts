b8f5329f42674e7434db0ad8b87d1357344a55fc
Rian Marlon <rianmarlon20@yahoo.com>
2020-11-20 00:27:53 -0300

    Adicionando o token nas configurações de requisição do axios do projeto mobile

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
   baseURL: 'http://192.168.1.6:3333',
+});
+
+api.interceptors.request.use(async (config: AxiosRequestConfig) => {
+  const token = await AsyncStorage.getItem('@happy:user');
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
